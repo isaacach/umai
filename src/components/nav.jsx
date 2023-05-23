8ee417000c8782f49d90852f942a1ab45ef0bbeb
isaacach <124728947+isaacach@users.noreply.github.com>
2023-05-22 22:28:15 -0500

    added logo and more links

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -8,13 +8,15 @@ export default function Nav() {
         <div className='col nav'>
           <div className='logo nav'>
             <h1>UMAI</h1>
-            <img src='./images/yummy.png' alt='yummy'/>
+            <img src='src/images/yummy.png' alt='yummy'/>
           </div>
         </div>
         <div className='col nav'>
           <div className='links nav'>
             <a><h3>About</h3></a>
             <a><h3>Menu</h3></a>
+            <a><h3>Favorites</h3></a>
+            <a><h3>Contact</h3></a>
           </div>
         </div>
       </div>
